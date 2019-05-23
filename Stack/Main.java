f22ff913283cfe22b3fdab6f28a831f556ae4caa
DavidHsin <daxian2392@gmail.com>
2019-05-23 23:34:52 +0800

    Update Main.java

--- Stack/Main.java
+++ Stack/Main.java
@@ -3,6 +3,23 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		//Demo1();
+		
+		//LinkedListStack测试
+		LinkedListStack<Integer> stack = new LinkedListStack<>();
+		
+		for(int i = 0; i < 5; i++) {
+			stack.push(i);
+			System.out.println(stack);
+		}
+		stack.pop();
+		System.out.println(stack);
+	}
+
+	/**
+	 * ArrayStack测试
+	 */
+	private static void Demo1() {
 		ArrayStack<Integer> stack = new ArrayStack<>();
 		
 		for(int i = 0; i < 5; i++) {
