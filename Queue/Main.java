57fceb8fef6dc83dbda1e166583eb7c33e7ce701
DavidHsin <daxian2392@gmail.com>
2019-05-23 23:34:15 +0800

    Update Main.java

--- Queue/Main.java
+++ Queue/Main.java
@@ -3,6 +3,27 @@ public class Main {
 
 	public static void main(String[] args) {
 		//demo1();
+		//Demo2();
+		
+		//测试LinkedListQueue
+		LinkedListQueue<Integer> queue = new LinkedListQueue<>();
+		for(int i = 0; i < 10; i++) {
+			queue.enqueue(i);
+			System.out.println(queue);
+			
+			//每插入三个元素，就取出一个元素
+			if(i % 3 == 2) {
+				queue.dequeue();
+				System.out.println(queue);
+			}
+		}
+		System.out.println(queue.getFront());
+	}
+
+	/**
+	 * 测试LoopQueue
+	 */
+	private static void Demo2() {
 		LoopQueue<Integer> queue = new LoopQueue<>();
 		for(int i = 0; i < 10; i++) {
 			queue.enqueue(i);
