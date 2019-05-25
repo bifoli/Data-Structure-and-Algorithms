ded0ffa8814a15dbf91abff40f231b1133d69945
DavidHsin <daxian2392@gmail.com>
2019-05-25 11:37:42 +0800

    Update LinkedListUsingDummyHead.java

--- LinkedList/LinkedListUsingDummyHead.java
+++ LinkedList/LinkedListUsingDummyHead.java
@@ -170,6 +170,23 @@ public class LinkedListUsingDummyHead<E> {
 		return remove(size - 1);
 	}
 	
+	//从链表中删除元素e，只是删除其中一个找到的元素e并不是删除所有
+	public void removeElement(E e) {
+		Node prev = dummyHead;
+		while(prev.next != null) {
+			if(prev.next.e.equals(e)) {
+				break;
+			}
+			prev = prev.next;
+		}
+		
+		if(prev.next != null) {
+			Node delNode = prev.next;
+			prev.next = delNode.next;
+			delNode.next = null;
+		}
+	}
+	
 	@Override
 	public String toString() {
 		
