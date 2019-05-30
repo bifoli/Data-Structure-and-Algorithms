938488fa4f2c8f542cea13036033f2c182445487
DavidHsin <daxian2392@gmail.com>
2019-05-30 12:11:36 +0800

    Create Main.java

---
+++ Trie/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> words = new ArrayList<>();
+		words.add("amazon");
+		words.add("blackrock");
+		words.add("cat");
+		words.add("dog");
+		words.add("facebook");
+		words.add("google");
+		words.add("highland");
+		words.add("itunes");
+		words.add("janestreet");
+		words.add("kirkland");
+		//words.add("pan");
+		words.add("panda");
+		System.out.println(words.contains("black"));
+	}
+
+}
