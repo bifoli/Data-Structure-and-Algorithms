bc53ceccdf82abc08f1275d35e3b4472cd567db6
DavidHsin <daxian2392@gmail.com>
2019-05-31 16:07:00 +0800

    Update UnionFind5.java

--- UnionFind/UnionFind5.java
+++ UnionFind/UnionFind5.java
@@ -12,71 +12,67 @@
  * 4
 */
 public class UnionFind5 implements UF {
-
-    // rank[i]表示以i为根的集合所表示的树的层数
-    // 在后续的代码中, 我们并不会维护rank的语意, 也就是rank的值在路径压缩的过程中, 有可能不在是树的层数值
-    // 这也是我们的rank不叫height或者depth的原因, 他只是作为比较的一个标准
-    private int[] rank;
-    private int[] parent; // parent[i]表示第i个元素所指向的父节点
-
-    // 构造函数
-    public UnionFind5(int size){
-
-        rank = new int[size];
-        parent = new int[size];
-
-        // 初始化, 每一个parent[i]指向自己, 表示每一个元素自己自成一个集合
-        for( int i = 0 ; i < size ; i ++ ){
-            parent[i] = i;
-            rank[i] = 1;
-        }
-    }
-
-    @Override
-    public int getSize(){
-        return parent.length;
-    }
-
-    // 查找过程, 查找元素p所对应的集合编号
-    // O(h)复杂度, h为树的高度
-    private int find(int p){
-        if(p < 0 || p >= parent.length)
-            throw new IllegalArgumentException("p is out of bound.");
-
-        while( p != parent[p] ){
-            parent[p] = parent[parent[p]];
-            p = parent[p];
-        }
-        return p;
-    }
-
-    // 查看元素p和元素q是否所属一个集合
-    // O(h)复杂度, h为树的高度
-    @Override
-    public boolean isConnected( int p , int q ){
-        return find(p) == find(q);
-    }
-
-    // 合并元素p和元素q所属的集合
-    // O(h)复杂度, h为树的高度
-    @Override
-    public void unionElements(int p, int q){
-
-        int pRoot = find(p);
-        int qRoot = find(q);
-
-        if( pRoot == qRoot )
-            return;
-
-        // 根据两个元素所在树的rank不同判断合并方向
-        // 将rank低的集合合并到rank高的集合上
-        if( rank[pRoot] < rank[qRoot] )
-            parent[pRoot] = qRoot;
-        else if( rank[qRoot] < rank[pRoot])
-            parent[qRoot] = pRoot;
-        else{ // rank[pRoot] == rank[qRoot]
-            parent[pRoot] = qRoot;
-            rank[qRoot] += 1;   // 此时, 我维护rank的值
-        }
-    }
+	
+	private int[] parent;
+	private int[] rank;		//rank[i]表示以i为根的集合所表示的树的层数
+	
+	public UnionFind5(int size) {
+		parent = new int[size];
+		rank = new int[size];
+		
+		for(int i = 0; i < size; i++) {
+			parent[i] = i;	//初始化每个节点都指向它自己，都是一棵独立的树
+			rank[i] = 1;	//对于一个节点来说，它的层数也是1
+		}
+	}
+	
+	@Override
+	public int getSize() {
+		return parent.length;
+	}
+	
+	//查找过程，比如此时需要查找节点6，那最终返回的是根节点1的集合编号1
+	//时间复杂度为O(h)，h为树的高度
+	private int find(int p) {
+		if(p < 0 && p > parent.length) {
+			throw new IllegalArgumentException("p is out of bound.");
+		}
+		
+		while(p != parent[p]) {
+			//比如查找节点6，此时节点6的parent是5，那就一直往上查找，知道根节点1的parent是1，查找停止
+			parent[p] = parent[parent[p]];
+			p = parent[p];
+		}
+		return p;
+	}
+	
+	//查看元素p和元素q是否所属一个集合
+	//O(h)时间复杂度，h为树的高度
+	@Override
+	public boolean isConnected(int p, int q) {
+		return find(p) == find(q);
+	}
+	
+	//合并元素p和元素q所属的集合
+	//O(h)时间复杂度，h为树的高度
+	@Override
+	public void unionElements(int p, int q) {
+		int pRoot = find(p);
+		int qRoot = find(q);
+		
+		if(pRoot == qRoot) {
+			return;
+		}
+		
+		//根据两个元素所在树的rank不同判断合并方向
+		//将rank低的集合合并到rank高的集合上
+		if(rank[pRoot] < rank[qRoot]) {
+			parent[pRoot] = qRoot;	//此时不用维护rank，因为将小的指向大的，那大的肯定不用维护size
+		}else if(rank[qRoot] < rank[pRoot]) {
+			parent[qRoot] = pRoot;	//同理
+		}else {
+			parent[qRoot] = pRoot;	//当这两棵树高度相等的时候，比如union(0,1)，此时0节点和1节点都是根节点，这时候rank就要变成2
+			rank[pRoot] += 1;		//注意此处是加1，而不是加rank[qRoot]，因为不是链表结构而是树结构
+		}
+	}
 }
