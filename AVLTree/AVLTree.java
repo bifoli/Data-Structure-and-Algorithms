656600f23fa9074bf8071fb637f63e5372312734
DavidHsin <daxian2392@gmail.com>
2019-06-04 13:21:26 +0800

    Update AVLTree.java

--- AVLTree/AVLTree.java
+++ AVLTree/AVLTree.java
@@ -30,7 +30,7 @@ public class AVLTree<K extends Comparable<K>, V> {
         public Node left, right;
         public int height;	//存储每一个节点的高度值，默认为1
 
-        public Node(K key, V value){
+        public Node(K key, V value) {
             this.key = key;
             this.value = value;
             left = null;
@@ -42,16 +42,16 @@ public class AVLTree<K extends Comparable<K>, V> {
     private Node root;
     private int size;
 
-    public AVLTree(){
+    public AVLTree() {
         root = null;
         size = 0;
     }
 
-    public int getSize(){
+    public int getSize() {
         return size;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
     }
     
@@ -153,6 +153,7 @@ public class AVLTree<K extends Comparable<K>, V> {
      *            / \                                      / \
      *           T2  T3                                   T3  T4
      * 
+     * 
      * */
     
     //右旋转的操作
@@ -190,25 +191,26 @@ public class AVLTree<K extends Comparable<K>, V> {
     	return x;
     }
     
-    public void add(K key, V value){
+    public void add(K key, V value) {
         root = add(root, key, value);
     }
 
     // 向以node为根的二分搜索树中插入元素(key, value)，递归算法
     // 返回插入新节点后二分搜索树的根
-    private Node add(Node node, K key, V value){
+    private Node add(Node node, K key, V value) {
 
-        if(node == null){
+        if(node == null) {
             size ++;
             return new Node(key, value);
         }
 
-        if(key.compareTo(node.key) < 0)
-            node.left = add(node.left, key, value);
-        else if(key.compareTo(node.key) > 0)
-            node.right = add(node.right, key, value);
-        else // key.compareTo(node.key) == 0
-            node.value = value;		//相等的话更新value
+        if(key.compareTo(node.key) < 0) {
+        	node.left = add(node.left, key, value);
+        }else if(key.compareTo(node.key) > 0) {
+        	node.right = add(node.right, key, value);
+        }else {	// key.compareTo(node.key) == 0
+        	node.value = value;		//相等的话更新value
+        }
         
         //更新height值
         node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
@@ -248,30 +250,31 @@ public class AVLTree<K extends Comparable<K>, V> {
     }
 
     // 返回以node为根节点的二分搜索树中，key所在的节点
-    private Node getNode(Node node, K key){
+    private Node getNode(Node node, K key) {
 
         if(node == null)
             return null;
 
-        if(key.equals(node.key))
+        if(key.equals(node.key)) {
             return node;
-        else if(key.compareTo(node.key) < 0)
+        }else if(key.compareTo(node.key) < 0) {
             return getNode(node.left, key);
-        else // if(key.compareTo(node.key) > 0)
-            return getNode(node.right, key);
+        }else {	// if(key.compareTo(node.key) > 0)
+        	return getNode(node.right, key);
+        }   
     }
 
-    public boolean contains(K key){
+    public boolean contains(K key) {
         return getNode(root, key) != null;
     }
 
-    public V get(K key){
+    public V get(K key) {
 
         Node node = getNode(root, key);
         return node == null ? null : node.value;
     }
 
-    public void set(K key, V newValue){
+    public void set(K key, V newValue) {
         Node node = getNode(root, key);
         if(node == null)
             throw new IllegalArgumentException(key + " doesn't exist!");
@@ -280,80 +283,105 @@ public class AVLTree<K extends Comparable<K>, V> {
     }
 
     // 返回以node为根的二分搜索树的最小值所在的节点
-    private Node minimum(Node node){
+    private Node minimum(Node node) {
         if(node.left == null)
             return node;
         return minimum(node.left);
     }
 
-    // 删除掉以node为根的二分搜索树中的最小节点
-    // 返回删除节点后新的二分搜索树的根
-    private Node removeMin(Node node){
-
-        if(node.left == null){
-            Node rightNode = node.right;
-            node.right = null;
-            size --;
-            return rightNode;
-        }
-
-        node.left = removeMin(node.left);
-        return node;
-    }
-
     // 从二分搜索树中删除键为key的节点
-    public V remove(K key){
+    public V remove(K key) {
 
         Node node = getNode(root, key);
-        if(node != null){
+        if(node != null) {
             root = remove(root, key);
             return node.value;
         }
         return null;
     }
 
-    private Node remove(Node node, K key){
+    private Node remove(Node node, K key) {
 
-        if( node == null )
-            return null;
-
-        if( key.compareTo(node.key) < 0 ){
+        if( node == null ) {
+        	return null;
+        }  
+        
+        Node retNode;
+        if( key.compareTo(node.key) < 0 ) {
             node.left = remove(node.left , key);
-            return node;
+            retNode = node;
         }
-        else if(key.compareTo(node.key) > 0 ){
+        else if(key.compareTo(node.key) > 0 ) {
             node.right = remove(node.right, key);
-            return node;
+            retNode = node;
         }
         else{   // key.compareTo(node.key) == 0
 
             // 待删除节点左子树为空的情况
-            if(node.left == null){
+            if(node.left == null) {
                 Node rightNode = node.right;
                 node.right = null;
                 size --;
-                return rightNode;
+                retNode = rightNode;
             }
 
             // 待删除节点右子树为空的情况
-            if(node.right == null){
+            else if(node.right == null) {
                 Node leftNode = node.left;
                 node.left = null;
                 size --;
-                return leftNode;
+                retNode = leftNode;
             }
 
-            // 待删除节点左右子树均不为空的情况
-
-            // 找到比待删除节点大的最小节点, 即待删除节点右子树的最小节点
-            // 用这个节点顶替待删除节点的位置
-            Node successor = minimum(node.right);
-            successor.right = removeMin(node.right);
-            successor.left = node.left;
-
-            node.left = node.right = null;
-
-            return successor;
+            else {// 待删除节点左右子树均不为空的情况
+	
+	            // 找到比待删除节点大的最小节点, 即待删除节点右子树的最小节点
+	            // 用这个节点顶替待删除节点的位置
+	            Node successor = minimum(node.right);
+	            successor.right = remove(node.right, successor.key);
+	            successor.left = node.left;
+	
+	            node.left = node.right = null;
+	
+	            retNode = successor;
+            }
         }
+        
+        if(retNode == null) {
+        	return null;
+        }
+        
+        //更新height值
+        retNode.height = 1 + Math.max(getHeight(retNode.left), getHeight(retNode.right));
+        
+        //计算平衡因子
+        int balanceFactor = getBalanceFactor(retNode);
+        
+        //平衡维护
+        //LL
+        if(balanceFactor > 1 && getBalanceFactor(retNode.left) >= 0) {
+        	return rightRotate(retNode);
+        }
+        
+        //RR
+        if(balanceFactor < -1 && getBalanceFactor(retNode.right) <= 0) {
+        	return leftRotate(retNode);
+        }
+        
+        //LR
+        if(balanceFactor > 1 && getBalanceFactor(retNode.left) < 0) {
+        	
+        	retNode.left = leftRotate(retNode.left);
+        	return rightRotate(retNode);
+        }
+        
+        //RL
+        if(balanceFactor < -1 && getBalanceFactor(retNode.right) > 0) {
+        	
+        	retNode.right = rightRotate(retNode.right);
+        	return leftRotate(retNode);
+        }
+        
+        return retNode;
     }
 }
